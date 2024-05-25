b5bfd71d364b69863ca962453cea219ca8d85fbd
olegonyxov <olegonyxov@gmail.com>
2024-05-25 18:59:02 +0300

    3

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+    return (
+        <header className="App-header" id="header">
+        <img src="http://localhost:3000/logo192.png" className="App-logo" alt="logo" />
+        <p> 
+          <strong>Welcome to HillelHW32 app </strong>          
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    )
+}
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import MainContent1 from './MainContent1';
+import MainContent2 from './MainContent2';
+import MainContent3 from './MainContent3';
+
+function Main(){
+    const [contentView, setView] = useState("content1");
+
+    const handleButtonClick = (content) => {
+        setView(content);
+    };
+
+    return (
+        <div className="Main-contaner">
+            <ul className='Nav-list'>
+                <li>
+                    <input type="button" value='Content1' onClick={() => handleButtonClick("content1")} />
+                </li>
+                <li>
+                    <input type="button" value='Content2' onClick={() => handleButtonClick("content2")} />
+                </li>
+                <li>
+                    <input type="button" value='Content3' onClick={() => handleButtonClick("content3")} />
+                </li>
+            </ul>
+            {contentView === "content1" ? <MainContent1 /> : contentView === "content2" ? <MainContent2 /> : <MainContent3 />}
+        </div>
+    );
+}
+
+export default Main;
